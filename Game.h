05d2a08846f9410354b40a2902a8241b08530b05
khang <43407475+khangneko@users.noreply.github.com>
2021-03-08 20:38:55 +0700

    Game loop

---
+++ Game.h
@@ -0,0 +1,27 @@
+#ifndef GAME_H_INCLUDED
+#define GAME_H_INCLUDED
+
+#include "SDL.h"
+#include <iostream>
+
+class Game {
+public:
+    Game(const char* title, int xpos, int ypos, int width, int height, bool fullscreen);
+    ~Game();
+    void init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen);
+
+    void handleEvents();
+    void update();
+    void render();
+    void clean();
+    bool running();
+
+private:
+    int count = 0;
+    bool isRunning;
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+};
+
+
+#endif // GAME_H_INCLUDED
--- main.cpp
+++ main.cpp
@@ -1,10 +1,20 @@
+// this is a test
 #include <exception>
 #include <string>
 #include <iostream>
 #include <SDL.h>
-using namespace std;
+#include "Game.h"
 
-int main(int argc, char* argv[]) {
-    cout << "hello world";
-    return 0;
+
+Game *game = nullptr;
+int main( int argc, char * argv[] )
+{
+    game = new Game("Pacman", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,800, 600, false);
+    while (game->running()) {
+        game->handleEvents();
+        game->update();
+        game->render();
+    }
+    game->~Game();
+    return 1;
 }
